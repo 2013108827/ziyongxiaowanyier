d789286663508fb22aa83d3f308b4db373d53294
LBWNB <59857991+2013108827@users.noreply.github.com>
2021-07-31 23:17:10 +0800

    各种符号图标

---
+++ logo.py
@@ -0,0 +1,27 @@
+
+weatherSymbol = {
+    "晴天": ["☀", u'\u2600'],   #Black Sun Clear Weather
+    "阴天": ["☁", u'\u2601'],   #Cloudy Weather
+    "多云": ["⛅", u'\u26C5'],   #Sun Behind Cloud Partly Cloudy
+    "雨": ["🌧", u'\U0001F327'],   #Cloud With RainCloud With Rain
+    "雷雨": ["⛈", u'\u26C8'],   #Thunder Cloud And Rain Thunderstorm
+    "雪": ["🌨", u'\U0001F328'],   #Cloud With Snow Snow, Snow Showers
+    "雾": ["🌫", u'\U0001F32B'],   #	Fog
+    "温度计": ["🌡", u'\U0001F321'],   #Thermometer
+    "White Sun Sunny": ["☼", u'\u263C'],
+    "Sun": ["☉", u'\u2609'],
+    "White Sun With Small Cloud Mostly Sunny": ["🌤", u'\U0001F324'],
+    "White Sun Behind Cloud Mostly Cloudy": ["🌥", u'\U0001F325'],
+    "White Sun Behind Cloud With Rain Showers": ["🌦", u'\U0001F326'],
+    "Cloud With Lightning": ["🌩", u'\U0001F329'],
+    "Cloud With Tornado Twister": ["🌪", u'\U0001F32A'],
+    "Umbrella Rainy Weather": ["☂", u'\u2602'],
+    "Umbrella Showery Weather": ["☔", u'\u2614'],
+    "Foggy": ["🌁", u'\U0001F301'],
+    "Lightning": ["☇", u'\u2607'],
+    "Thunderstorm": ["☈", u'\u2608'],
+    "Snowman Snowy Weather": ["☃", u'\u2603'],
+    "Snowman Light Snow": ["⛄", u'\u26C4'],
+    "Black Snowman Heavy Snow": ["⛇", u'\u26C7'],
+    "Comet": ["☄", u'\u2604'],
+}
