20b654c9871d4f78816bd83386816aa70523f33c
LBWNB <59857991+2013108827@users.noreply.github.com>
2021-08-01 12:37:17 +0800

    Update logo.py

--- logo.py
+++ logo.py
@@ -1,3 +1,8 @@
+'''
+符号图标合集。
+合集名称={"名字"：["图形", unicode编码], #英文名字}
+'''
+
 
 weatherSymbol = {
     "晴天": ["☀", u'\u2600'],   #Black Sun Clear Weather
@@ -6,6 +11,9 @@ weatherSymbol = {
     "雨": ["🌧", u'\U0001F327'],   #Cloud With RainCloud With Rain
     "雷雨": ["⛈", u'\u26C8'],   #Thunder Cloud And Rain Thunderstorm
     "雪": ["🌨", u'\U0001F328'],   #Cloud With Snow Snow, Snow Showers
+    "小雪": ["❄", u'\U00010052'],   #Snowflake
+    "中雪": ["❅", u'\U00010053'],   #Tight Snowflake
+    "大雪": ["❆", u'\U00010054'],   #Heavy Snowflake
     "雾": ["🌫", u'\U0001F32B'],   #	Fog
     "温度计": ["🌡", u'\U0001F321'],   #Thermometer
     "White Sun Sunny": ["☼", u'\u263C'],
@@ -24,4 +32,23 @@ weatherSymbol = {
     "Snowman Light Snow": ["⛄", u'\u26C4'],
     "Black Snowman Heavy Snow": ["⛇", u'\u26C7'],
     "Comet": ["☄", u'\u2604'],
+}
+
+arrowSymbol	= {
+    "向上箭头": ["↑", u'\u2191'],   #Upwards Arrow
+    "向下箭头": ["↓", u'\u2193'],   #Downwards Arrow
+    "向右箭头": ["→", u'\u2192'],   #	Rightwards Arrow
+    "向左箭头": ["←", u'\u2190'],   #Leftwards Arrow
+    "左右箭头": ["↔", u'\u2194'],   #Left-Right Arrow
+    "上下箭头": ["↕", u'\u2195'],   #Up-Down Arrow
+    "带底边的上下箭头": ["↨", u'\u21a8'],   #Up-Down Arrow With Base
+    "向上实心三角": ["▲", u'\u25b2'],   #Upwards filled arrow
+    "向下实心三角": ["▼", u'\u25bc'],   #Downwards filled arrow
+    "向右实心三角": ["►", u'\u25ba'],   #Rightwards filled arrow
+    "向左实心三角": ["◄", u'\u25c4'],   #Leftwards filled arrow
+}
+
+speciaSymbol = {
+    "警告": ["⚠", u'\u26a0'],   #Warning Sign
+    "城市": ["🏙", u'\U0001F3D9'],  #city
 }
